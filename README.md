4eff256966689cd1c519beca67c82cd12d43bc9e
serialworm <eric.beringer@gmail.com>
2015-11-01 10:09:43 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # gmail-empty-trash
-Empty your Gmail Trash on a regular basis
+Simple script to empty your gmail trash
+
+Since the setting to automatically delete trash is not configurable
+and only happens every 30 days, you can use this to empty your trash
+on a more regular basis using a crontask.
+
+## Usage
+
+```shell
+$ pip install --upgrade google-api-python-client
+$ python empty-trash.py
+```
+
+Requires Python 3
---
+++ empty-trash.py
@@ -0,0 +1,79 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+from apiclient import errors
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://mail.google.com/'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail Empty Trash'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'gmail-empty-trash.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
+def main():
+    """Empty your gmail trash.
+
+    Since the setting to automatically delete trash is not configurable
+    and only happens every 30 days, you can use this to empty your trash
+    on a more regular basis using a crontask.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    try:
+        response = service.users().threads().list(
+            userId='me', q='label:TRASH').execute()
+        threads = []
+        if 'threads' in response:
+            threads.extend(response['threads'])
+            for thread in threads:
+                try:
+                    service.users().messages().delete(
+                        userId='me', id=thread['id']).execute()
+                    print('Message with id: %s deleted successfully.' %
+                          thread['id'])
+                except errors.HttpError as error:
+                    print('An error occurred: %s' % error)
+
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+
+if __name__ == '__main__':
+    main()
